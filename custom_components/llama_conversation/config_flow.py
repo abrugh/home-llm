962cad64ba721ecb21bc0162b93d70f3bb83ca77
Alexander Brugh <abrugh@server.local>
2025-02-12 19:52:52 -0700

    trying to handle azure openai

--- custom_components/llama_conversation/config_flow.py
+++ custom_components/llama_conversation/config_flow.py
@@ -508,6 +508,11 @@ class ConfigFlow(BaseLlamaConversationConfigFlow, config_entries.ConfigFlow, dom
 
         self.download_task = None
         return self.async_show_progress_done(next_step_id=next_step)
+
+    async def _async_validate_azure_openai(self, user_input: dict) -> tuple:
+        user_input[CONF_GENERIC_OPENAI_PATH] = user_input.get(CONF_GENERIC_OPENAI_PATH, "")
+        return await _async_validate_generic_openai(self, user_input)
+
     
     async def _async_validate_generic_openai(self, user_input: dict) -> tuple:
         """
@@ -647,6 +652,9 @@ class ConfigFlow(BaseLlamaConversationConfigFlow, config_entries.ConfigFlow, dom
                 elif backend_type == BACKEND_TYPE_GENERIC_OPENAI and \
                     user_input.get(CONF_GENERIC_OPENAI_VALIDATE_MODEL, DEFAULT_GENERIC_OPENAI_VALIDATE_MODEL):
                      error_message, ex, possible_models = await self._async_validate_generic_openai(user_input)
+                elif backend_type = BACKEND_TYPE_AZURE_OPENAI and \
+                    user_input.get(CONF_AZURE_OPENAI_VALIDATE_MODEL, DEFAULT_AZURE_OPENAI_VALIDATE_MODEL):
+                     error_message, ex, possible_models = await self._async_validate_azure_openai(user_input)
                 else:
                     possible_models = []
 
